91ba516a91c5a8e9cb799fc42b61fe27eb7e7e47
jjviasus <61569318+jjviasus@users.noreply.github.com>
2021-12-19 18:39:41 -0500

    Observing Notifications

--- Mooskine/Model/DataController.swift
+++ Mooskine/Model/DataController.swift
@@ -20,9 +20,22 @@ class DataController {
         return persistentContainer.viewContext // the viewContext is associated with the main queue
     }
     
+    var backgroundContext: NSManagedObjectContext!
+    
     init(modelName: String) {
         // we need the name of the data model file
         persistentContainer = NSPersistentContainer(name: modelName)
+    }
+    
+    func configureContexts() {
+        // instantiate the background context
+        backgroundContext = persistentContainer.newBackgroundContext() // creates a context associated with a private queue
+        
+        viewContext.automaticallyMergesChangesFromParent = true
+        backgroundContext.automaticallyMergesChangesFromParent = true
+        
+        backgroundContext.mergePolicy = NSMergePolicy.mergeByPropertyObjectTrump // it will prefer its own property values in the case of a conflict
+        viewContext.mergePolicy = NSMergePolicy.mergeByPropertyStoreTrump // it will prefer the property values from the persistent store in the case of a conflict
     }
     
     // takes in a completion as a parameter, which is optional and defaults to nil.
@@ -36,6 +49,7 @@ class DataController {
             }
             
             self.autoSaveViewContext()
+            self.configureContexts()
             
             // The passed in function gets called after loading the store.
             completion?()
@@ -63,3 +77,28 @@ extension DataController {
         }
     }
 }
+
+// // creates a background context that sticks around
+// let backgroundContext = persistentContainer.newBackgroundContext()
+//
+// These 3 methods below are invaluable, you should always wrap your context's tasks in a perform call. If you don't, using a context on the wrong queue won't always crash your app. Unfortunately, the errors it causes can be intermitten and hard to debug. Because of this XCode provides a debug flag you can add to your scheme that will cause your app to crash immediately if you access a context from the wrong queue.
+
+// To add the flag: Product -> Scheme -> Edit Scheme
+
+// The 1 in -com.apple.CoreData.ConcurrencyDebug 1 indicates the level of detail in the stack trace. You can bump this up to 2 or 3 for more verbose output. It's a great idea to leave this set during development or debugging to identify any code running on the wrong queue.
+
+//        // creates a temporary background context to perform a single piece of work (performs background task on the container)
+//        persistentContainer.performBackgroundTask { context in
+//            doSomeSlowWork()
+//            try? context.save()
+//        }
+//
+//        // dispatches asynchronously on the correct queue for that context
+//        viewContext.perform {
+//            doSomeWork()
+//        }
+//
+//        // dispatches synchronously on the correct queue (performs and waits on the context itself)
+//        viewContext.performAndWait {
+//            doSomeWork()
+//        }
--- Mooskine/View Controllers/NoteDetailsViewController.swift
+++ Mooskine/View Controllers/NoteDetailsViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 // This is where the user sees and edits individual notes.
 class NoteDetailsViewController: UIViewController {
@@ -17,6 +18,8 @@ class NoteDetailsViewController: UIViewController {
     var note: Note! // injected before view controller is displayed
     
     var dataController: DataController! // injected before view controller is displayed (previous view passes it to us during the segue)
+    
+    var saveObserverToken: Any?
 
     /// A closure that is run when the user asks to delete the current note
     var onDelete: (() -> Void)?
@@ -42,6 +45,12 @@ class NoteDetailsViewController: UIViewController {
         // keyboard toolbar configuration
         configureToolbarItems()
         configureTextViewInputAccessoryView()
+        
+        addSaveNotificationObserver()
+    }
+    
+    deinit {
+        removeSaveNotificationObserver()
     }
 
     @IBAction func deleteNote(sender: Any) {
@@ -85,8 +94,11 @@ extension NoteDetailsViewController {
     func makeToolbarItems() -> [UIBarButtonItem] {
         let trash = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(deleteTapped(sender:)))
         let space = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
-
-        return [trash, space]
+        let bold = UIBarButtonItem(image: #imageLiteral(resourceName: "toolbar-bold"), style: .plain, target: self, action: #selector(boldTapped(sender:)))
+        let red = UIBarButtonItem(image: #imageLiteral(resourceName: "toolbar-underline"), style: .plain, target: self, action: #selector(redTapped(sender:)))
+        let cow = UIBarButtonItem(image: #imageLiteral(resourceName: "toolbar-cow"), style: .plain, target: self, action: #selector(cowTapped(sender:)))
+        
+        return [trash, space, bold, red, cow]
     }
 
     /// Configure the current toolbar
@@ -108,6 +120,70 @@ extension NoteDetailsViewController {
     @IBAction func deleteTapped(sender: Any) {
         showDeleteAlert()
     }
+    
+    @IBAction func boldTapped(sender: Any) {
+        let newText = textView.attributedText.mutableCopy() as! NSMutableAttributedString
+        newText.addAttribute(.font, value: UIFont(name: "OpenSans-Bold", size: 22)!, range: textView.selectedRange)
+        
+        let selectedTextRange = textView.selectedTextRange
+        
+        textView.attributedText = newText
+        textView.selectedTextRange = selectedTextRange
+        note.attributedText = textView.attributedText
+        try? dataController.viewContext.save()
+    }
+    
+    @IBAction func redTapped(sender: Any) {
+        let newText = textView.attributedText.mutableCopy() as! NSMutableAttributedString
+        let attributes:[NSAttributedString.Key: Any] = [
+            .foregroundColor: UIColor.red,
+            .underlineStyle: 1,
+            .underlineColor: UIColor.red
+        ] // allows us to set multiple attributes at once
+        newText.addAttributes(attributes, range: textView.selectedRange)
+        
+        let selectedTextRange = textView.selectedTextRange
+        
+        textView.attributedText = newText
+        textView.selectedTextRange = selectedTextRange
+        note.attributedText = textView.attributedText
+        try? dataController.viewContext.save()
+    }
+    
+    @IBAction func cowTapped(sender: Any) {
+        let backgroundContext:NSManagedObjectContext! = dataController.backgroundContext
+        
+        let newText = textView.attributedText.mutableCopy() as! NSMutableAttributedString
+        
+        let selectedRange = textView.selectedRange
+        let selectedText = textView.attributedText.attributedSubstring(from: selectedRange)
+        
+        // Every managed object has an identifier that's consitent across contexts. We can access it through the objectID property.
+        
+        // We store the note's ID before we enter the perform block.
+        let noteID = note.objectID
+        
+        // Since UI kit isn't thread safe, we shouldn't access any UI elements from the background. So we won't include any UI code in the perform block. We will have all our foreground dependent code up top. We also can't access any viewContext objects in our perform block. The note instance is associated with the viewContext, so we can't use this note instance in the background. We need another note instance.
+        backgroundContext.perform {
+            // we need to get a matching note instance associated with the background context
+            let backgroundNote = backgroundContext.object(with: noteID) as! Note
+            
+            let cowText = Pathifier.makeMutableAttributedString(for: selectedText, withFont: UIFont(name: "AvenirNext-Heavy", size: 56)!, withPatternImage: #imageLiteral(resourceName: "texture-cow"))
+            newText.replaceCharacters(in: selectedRange, with: cowText)
+            
+            sleep(5)
+            
+            // update the attributed text of the background note to use the new text
+            backgroundNote.attributedText = newText
+            
+            // save the changes in the background context
+            try? backgroundContext.save()
+        }
+        
+//        // Update the UI
+//        textView.attributedText = newText
+//        textView.selectedRange = NSMakeRange(selectedRange.location, 1)
+    }
 
     // MARK: Helper methods for actions
     private func showDeleteAlert() {
@@ -122,5 +198,42 @@ extension NoteDetailsViewController {
         alert.addAction(cancelAction)
         alert.addAction(deleteAction)
         present(alert, animated: true, completion: nil)
+    }
+}
+
+// All we've done here is request to be notified when data in the view context has changed, so that we can handle that event and reload the text.
+extension NoteDetailsViewController {
+    
+    func removeSaveNotificationObserver() {
+        // check whether the saveObserverToken property has a value
+        if let token = saveObserverToken {
+            // remove it from notification center
+            NotificationCenter.default.removeObserver(token)
+        }
+    }
+    
+    func addSaveNotificationObserver() {
+        // Remove any existing observer
+        removeSaveNotificationObserver()
+        
+        // Set the token
+        saveObserverToken = NotificationCenter.default.addObserver(
+            forName: .NSManagedObjectContextObjectsDidChange, // we watch for this
+            object: dataController.viewContext, // on the view context
+            queue: nil, // run the block synchronously on the posting thread
+            using: handleSaveNotification(notification:)) // the block to run
+    }
+    
+    // reload the text
+    fileprivate func reloadText() {
+        textView.attributedText = note.attributedText
+    }
+    
+    // handles the notifications when they come
+    func handleSaveNotification(notification: Notification) {
+        // make sure we dispath this code on the main queue
+        DispatchQueue.main.async {
+            self.reloadText()
+        }
     }
 }
